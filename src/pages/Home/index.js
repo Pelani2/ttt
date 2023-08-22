45cce60225bafb9c5cfb5ee3d185c9063f32703a
Igor Zupunski <ipatheword@live.com>
2023-08-22 13:14:42 +0200

    finished board

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Board from "../../components/Board";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Board />
         </div>  
     );
 };
